70a3f3eb1c835ea4694ab648447d45cac9897887
Sofía Makita <sophiamakita@gmail.com>
2025-04-23 18:08:05 -0300

    adding assets and packages

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Logo from '../assets/logo.svg';
+
+    const Navbar = () => {
+        return (
+            <nav>
+                <img src={Logo} alt='sneakers store logo' />
+            </nav>
+        );
+    };
+
+    export default Navbar;
