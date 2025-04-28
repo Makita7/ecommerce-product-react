196b96df32e60414159305dd1e7322042776306f
Sofía Makita <sophiamakita@gmail.com>
2025-04-28 18:39:15 -0300

    making mobile navbar and menu modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,106 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Logo from '../assets/logo.svg';
+import ProfileIcon from '../assets/image-avatar.png';
+import CartIcon from '../assets/icon-cart.svg';
+import NavBurgerIcon from '../assets/icon-menu.svg';
+import styled from 'styled-components';
+
+    const NavbarItem = (name, index) =>{
+        return(
+            <p className='ps-2 mb-0' key={index} >{name}</p>
+        )
+    }
+
 
     const Navbar = () => {
+        const links = ['Collections', 'menubar', 'Women', 'About', 'Contact'];
+        const [isMobile, setIsMobile] = useState(false);
+
+        let [isCartToggle, setIsCartToggle] = useState(false);
+        const toggleMenu = () => {
+            setIsCartToggle(!isCartToggle);
+            console.log(isCartToggle);
+        }
+
+        useEffect(() => {
+            const checkScreen = () => {
+                setIsMobile(window.innerWidth < 768);
+            }
+            checkScreen();
+            window.addEventListener('resizes', checkScreen);
+
+        }, []);
+
         return (
-            <nav>
-                <img src={Logo} alt='sneakers store logo' />
-            </nav>
+            <>
+                <nav className='d-flex align-items-center justify-content-between navbar m-4 m-lg-0'>
+                    <div className='d-flex align-items-center'>
+                        { isMobile && <MenuIcon onClick={toggleMenu} src={NavBurgerIcon} alt='navigation menu button' className='me-2' /> }
+
+                        <img src={Logo} alt='sneakers store logo' className='ms-2 m-lg-0' />
+                        <div className='d-flex align-items-center ms-2'>
+                            { !isMobile && links.map((title, index) => (
+                                NavbarItem(title, index)
+                            ))}
+                        </div>
+                    </div>
+
+                    <div className='d-flex align-items-end'>
+                        <img src={CartIcon} alt='shopping cart button' />
+                        <ProfilePhoto src={ProfileIcon} alt='user profile mini photo' className='ms-4 pt-lg-2' />
+                    </div>
+                </nav>
+
+                { isCartToggle &&
+                    <OverlayStyle className={`inset-0 d-flex justify-content-center align-items-center`}>
+                        <ModalStyle className='py-2 px-3'>
+                            hi there
+                            <ModalActions>
+                                <button onClick={toggleMenu} className='btn mx-1' type='button' >close</button>
+                                <button onClick={toggleMenu} className='btn btn-primary ms-1' type='button' >close</button>
+                            </ModalActions>
+                        </ModalStyle>
+                    </OverlayStyle>
+                }
+            </>
         );
     };
 
     export default Navbar;
+
+
+const MenuIcon = styled.img`
+    width: 1.3rem;
+`;
+
+const ProfilePhoto = styled.img`
+    width: 2rem;
+    @media (min-width: 768px) {
+        width: 2.5rem;
+    }
+`;
+
+const OverlayStyle = styled.div`
+    background-color: #4d4d4d85;
+    height: 100%;
+    top: 0;
+    position: fixed;
+    width: 100%;
+    z-index: 1000;
+`;
+
+const ModalStyle = styled.div`
+    position: absolute;
+    box-shadow: 0px 7px 8px #00000052;
+    width: 80%;
+    height: 50%;
+    background: white;
+    border-radius: 1rem;
+`;
+
+const ModalActions = styled.div`
+    margin: 1rem;
+    bottom: 0;
+    position: absolute;
+    right: 0;
+`
