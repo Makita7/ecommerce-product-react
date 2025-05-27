b80c394fbd8ff5fe088119142cca93265de915c0
Sofía Makita <sophiamakita@gmail.com>
2025-05-27 10:35:05 -0300

    configs

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ecommerce-product-react/',
   server: {
     port: 5000
   },
